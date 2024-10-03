3db784f7f88e1c40e5f789605e3287e23296f58f
kusneid <kusneid@gmail.com>
2024-10-04 00:08:54 +0300

    2.1))

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-simple dotfiles for hyprland and its modules, terminal is kitty by default, for install execute ./install.sh
+simple dotfiles for hyprland, terminal is kitty by default, to install execute ./install.sh
 ![Example](image.png)
--- install.sh
+++ install.sh
@@ -4,14 +4,9 @@ CONFIG_DIR="$HOME/.config"
 
 echo "downloading packages:(YAY REQUIRED)"
 
-exec yay -S wlogout swaylock-effects
+yay -S wlogout swaylock-effects 
 
-exec sudo pacman -S dunst fish hyprland hypridle hyprpaper rofi-wayland waybar ttf-jetbrains-mono hyprlang hyprcursor dunst pavucontrol blueman nm-connection-editor 
-
-
-
-
-
+sudo pacman -S dunst fish hyprland hypridle wl-clipboard cliphist hyprpaper rofi-wayland waybar ttf-jetbrains-mono hyprlang hyprcursor dunst pavucontrol blueman nm-connection-editor 
 
 DOTFILES_DIR="$(pwd)" 
 
@@ -39,8 +34,6 @@ create_symlink2() {
     fi
 }
 
-
-
 files=("hypr" "dunst" "fish" "kitty" "rofi" "swaylock" "waybar")
 
 additional=("Sounds" "Wallpapers")
@@ -52,6 +45,5 @@ done
 for file in "${additional[@]}"; do
     create_symlink2 "$file"
 done
-
 
 echo "all done"
