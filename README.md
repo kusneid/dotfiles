6b8b34ceb269017cd84a5016305199289f32a9da
Vladislav Baydakov <kusneid@gmail.com>
2025-01-31 16:22:27 +0300

    NixOS migration

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-simple dotfiles for hyprland, terminal is kitty by default, to install execute ./install.sh, bluetooth and wifi applets located in tray, audio driver is pipewire, waybar for bar, dunst for notifications, hyprlock for locksreen, rofi(wayland version) for menu, hyprshot for screenshots
+dotfiles for hyprland, terminal is kitty by default, to install execute ./install.sh, bluetooth and wifi applets located in tray, audio driver is pipewire, waybar for bar, dunst for notifications, hyprlock for locksreen, rofi(wayland version) for menu, hyprshot for screenshots
+also there are my main and home-manager configs for nix-os and script for switching dark/light modes
 
-kde theme:
-https://github.com/vinceliuice/Layan-kde
-![text](preview.png)
+i really recommend to have a look on this dots(especially ./hypr/conf/binds.conf) before applying some of it(or just copy all dots to your system lol)
+
+![text](preview.jpg)
+![text](preview1.jpg)
---
+++ scripts/switch_mode.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+QT5_DIR="$HOME/dotfiles/qt5ct"
+QT6_DIR="$HOME/dotfiles/qt6ct"
+
+QT5_CONF="$QT5_DIR/qt5ct.conf"
+QT5_ALT_CONF="$QT5_DIR/qt5ct1.conf"
+
+QT6_CONF="$QT6_DIR/qt6ct.conf"
+QT6_ALT_CONF="$QT6_DIR/qt6ct1.conf"
+
+blackwallpaper="$HOME/Documents/Wallpapers/nebo-derevia-gory-tuchi-priroda-skaly-vodopad-new-zealand-vo.jpg"
+whitewallpaper="$HOME/Documents/Wallpapers/melanie-magdalena-KpBAYMNf9Tw-unsplash.jpg"
+
+if [ "$1" == "dark" ]; then
+    dconf write /org/gnome/desktop/interface/gtk-theme "'Orchis-Dark'"
+    dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+
+    if [ -f "$QT5_CONF.bak" ]; then
+        mv "$QT5_CONF" "$QT5_ALT_CONF"
+        mv "$QT5_CONF.bak" "$QT5_CONF"
+    fi
+
+    if [ -f "$QT6_CONF.bak" ]; then
+        mv "$QT6_CONF" "$QT6_ALT_CONF"
+        mv "$QT6_CONF.bak" "$QT6_CONF"
+    fi
+
+    sed -i 's/"workbench.colorTheme": "[^"]*"/"workbench.colorTheme": "Decay"/' ~/.config/Code/User/settings.json
+
+    hyprctl hyprpaper preload "$whitewallpaper"
+
+    hyprctl hyprpaper wallpaper "eDP-1,$whitewallpaper"
+    hyprctl hyprpaper wallpaper "HDMI-A-1,$whitewallpaper"
+
+elif [ "$1" == "light" ]; then
+     dconf write /org/gnome/desktop/interface/gtk-theme "'Orchis-Light'"
+    dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
+
+
+    if [ -f "$QT5_ALT_CONF" ]; then
+        mv "$QT5_CONF" "$QT5_CONF.bak"
+        mv "$QT5_ALT_CONF" "$QT5_CONF"
+    fi
+
+    if [ -f "$QT6_ALT_CONF" ]; then
+        mv "$QT6_CONF" "$QT6_CONF.bak"
+        mv "$QT6_ALT_CONF" "$QT6_CONF"
+    fi
+
+    sed -i 's/"workbench.colorTheme": "[^"]*"/"workbench.colorTheme": "Light Decay"/' ~/.config/Code/User/settings.json
+
+    hyprctl hyprpaper preload "$blackwallpaper"
+
+    hyprctl hyprpaper wallpaper "eDP-1,$blackwallpaper"
+    hyprctl hyprpaper wallpaper "HDMI-A-1,$blackwallpaper"
+
+else
+    echo "Использование: $0 [dark|light]"
+    exit 1
+fi
